ae1de74f64eaec6745ac28c438ae5c4f4c1d3645
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-16 00:50:44 -0600

    feat: :sparkles: create new employee

--- src/controllers/employees/employees.controller.js
+++ src/controllers/employees/employees.controller.js
@@ -1,11 +1,30 @@
 import { pool } from "../../db.js"
 
 export const getAllEmployees = async(req, res)=>{
+
     const [rows] = await pool.query("SELECT * FROM employees");
     res.json(rows);
 };
 
 export const getEmployeeById = async(req, res)=>{
+    
     const [rows] = await pool.query("SELECT * FROM employees WHERE id = ?", [req.params.id]);
+
+    if(rows.length === 0){
+        return res.status(404).json({ message: `Employee does not exists` });
+    }
+
     res.json(rows[0]);
+}
+
+export const createNewEmployee = async(req, res)=>{
+
+    const { name, salary } = req.body;
+    const [rows] = await pool.query("INSERT INTO employees (name, salary) VALUES (?,?)", [name, salary]);
+
+    res.json({
+        id: rows.insertId,
+        name,
+        salary
+    });
 }
--- src/routes/employees/employees.routes.js
+++ src/routes/employees/employees.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getAllEmployees, getEmployeeById } from "../../controllers/employees/employees.controller.js";
+import { getAllEmployees, getEmployeeById, createNewEmployee } from "../../controllers/employees/employees.controller.js";
 
 const router = express.Router();
 
 export const getEmployees = router.get("/employees", getAllEmployees);
 export const getEmployee = router.get("/employees/:id", getEmployeeById);
-export const createEmployee = router.post("/employees", getAllEmployees);
+export const createEmployee = router.post("/employees", createNewEmployee);
 export const updateEmployee = router.put("/employees", getAllEmployees);
 export const deleteEmployee = router.get("/employees", getAllEmployees);
