b3b5b84514fe732042e35abe3b874beeb634ef03
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-18 00:33:17 -0600

    fix: :bug: update port env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js';
 import { APP_PORT } from './config.js';
 
-app.listen(APP_PORT);
-console.log('Server running on port', APP_PORT);
+app.listen(PORT);
+console.log('Server running on port', PORT);
